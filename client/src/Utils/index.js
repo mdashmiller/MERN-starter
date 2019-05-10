d2d33bb799b6b5d0086c61619b754fdd00da747f
mdashmiller <m.robert.miller@gmail.com>
2019-05-10 12:31:16 -0500

    add integration test for getItems action

--- client/src/Utils/index.js
+++ client/src/Utils/index.js
@@ -1,7 +1,15 @@
 import checkPropTypes from 'check-prop-types'
+import { applyMiddleware, createStore } from 'redux'
+import rootReducer from '../store/reducers'
+import { middlewares } from '../store'
 
 export const findByTestAttr = (component, attr) =>
   component.find(`[data-test='${attr}']`)
 
 export const checkProps = (component, expectedProps) =>
   checkPropTypes(component.propTypes, expectedProps, 'props', component.name)
+
+export const testStore = intialState => {
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+  return createStoreWithMiddleware(rootReducer, intialState)
+}
---
+++ client/src/_integrationTests/spec.js
@@ -0,0 +1,48 @@
+import { getItems } from '../store/actions/itemActions'
+import moxios from 'moxios'
+import { testStore } from '../Utils'
+
+describe('getItems action', () => {
+
+  beforeEach(() =>{
+    moxios.install()
+  })
+
+  afterEach(() => {
+    moxios.uninstall()
+  })
+
+  it('should update the store correctly', () => {
+    const expectedState = [
+      {
+        title: 'test title 1',
+        body: 'test content 1'
+      },
+      {
+        title: 'test title 2',
+        body: 'test content 2'
+      },
+      {
+        title: 'test title 3',
+        body: 'test content 3'
+      }
+    ]
+    const store = testStore()
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({
+        status: 200,
+        response: expectedState
+      })
+    })
+
+    return store.dispatch(getItems())
+      .then(() => {
+        const newState = store.getState()
+
+        expect(newState.items).toBe(expectedState)
+      })
+  })
+
+})
